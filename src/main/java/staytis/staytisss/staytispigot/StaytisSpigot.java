a76a3d02bd2d951f21ac82b87d6876666322497b
Staytis <158001259+Staytis@users.noreply.github.com>
2024-01-28 21:32:58 -0300

    Commit Profiles

---
+++ src/main/java/staytis/staytisss/staytispigot/StaytisSpigot.java
@@ -0,0 +1,50 @@
+package st.staytis.staytisspigot;
+
+
+import org.bukkit.Bukkit;
+import org.bukkit.Sound;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+
+public final class StaytisSpigot extends JavaPlugin implements CommandExecutor {
+
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginCommand("sound").setExecutor(this);
+    }
+
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(!(sender instanceof Player)) return false;
+        if(args.length<1) return false;
+        String soundName = args[0].toUpperCase();
+        Player player = (Player) sender;
+        ((Player) sender).playSound(player.getLocation(), Sound.valueOf(soundName), 1 ,1);
+        //Ainda não testei :p
+
+        return true;
+    }
+}
+
+
+
+
+
+
+
